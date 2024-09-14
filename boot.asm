97ff630e8b199a7a598f4eb2939da57fe5d10b20
rendemare <rendemare@gmail.com>
2024-09-14 21:48:57 +0700

    hihi

--- boot.asm
+++ boot.asm
@@ -1,26 +1,28 @@
 [BITS 16]
 
+cli
 mov ax, 0x7C0
+mov sp, ax 
 mov es, ax
 mov ds, ax
+xor ax, ax
+mov ss, ax
 mov bp, msg
-
-mov ax, 0x7
-int 0x10
+sti
 
 mov ah, 0x13
-mov bl, 0x1 
+mov bl, 0x1
 mov al, 1
-mov cx, [msg_length]
-
+mov cx, msg_length
+mov dh, 16 
+mov dl, 0 
 int 0x10
 
-hlt
+loop:
+    jmp loop
 
 msg db 'Hello, World!'
-msg_length dw $-msg
-msg_x dw 0
-msg_y dw 0
+msg_length equ $-msg
 
 times  510 - ($-$$) db 0
 dw 0xaa55
