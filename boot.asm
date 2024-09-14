f6d682b6e1ed7dafbb1aa5e39cb5916cfbd4f0f9
rendemare <rendemare@gmail.com>
2024-09-15 00:11:26 +0700

    20b hello world

--- boot.asm
+++ boot.asm
@@ -2,14 +2,13 @@
 
 cli
 mov sp, 0x7C0
-mov ds, sp
 mov ss, sp 
 
 mov ah, 0xe
 mov si, msg
 
 loop:
-    lodsb
+    ss lodsb
     int 0x10
     test al, al
     jnz loop
@@ -17,7 +16,7 @@ loop:
 hlt
 
 msg db 'Hello, World!', 0
-msg_length equ $-msg
 
 times  510 - ($-$$) db 0
 dw 0xaa55
+
