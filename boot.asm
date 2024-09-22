5c4d8c4d72b3c8d8bcded2a255f09eff8c0066d8
Reshetnikova Natalia <nataliareshetnikova943@gmail.com>
2024-09-22 21:05:09 +0700

    add comments

--- boot.asm
+++ boot.asm
@@ -1,22 +1,27 @@
 [BITS 16]
 
+; clear interrupt-enable flag
 cli
-mov sp, 0x7C0
-mov ss, sp 
-
+mov ax, 0x7c0 
+mov ss, ax
+xor sp, sp
 mov ah, 0xe
 mov si, msg
 
 loop:
-    ss lodsb
-    int 0x10
-    test al, al
-    jnz loop
+  ; read byte from ss:si into al, si++
+  ss lodsb
+  ; ah = 0xe (display char), al = char
+  int 0x10
+  ; subtracting and setting flags
+  test al, al
+  jnz loop
 
+; stop instruction execution
 hlt
 
-msg db 'Hello, World!', 0
-
-times  510 - ($-$$) db 0
+msg:
+  db "Hello world!", 0
+; generate zero bytes to size 510
+times 510 - ($ - $$) db 0 
 dw 0xaa55
-
