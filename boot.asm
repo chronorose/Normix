e064b5f40298019e34a57da033288ea09c86ba67
Reshetnikova Natalia <nataliareshetnikova943@gmail.com>
2024-09-23 15:25:29 +0700

    fix

--- boot.asm
+++ boot.asm
@@ -14,15 +14,14 @@ loop:
   ss lodsb
   ; ah = 0xe (display char), al = char
   int 0x10
-  ; subtracting and setting flags
+  ; logical AND and setting flags
   test al, al
   jnz loop
 
 ; stop instruction execution
 hlt
 
-msg:
-  db 'Hello, World!', 0
+msg db 'Hello, World!', 0
 
 ; generate zero bytes to size 510
 times  510 - ($ - $$) db 0
