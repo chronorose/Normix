d54e20976693298fae6fdac2e36a8674af0509f8
Reshetnikova Natalia <nataliareshetnikova943@gmail.com>
2024-10-10 20:47:55 +0700

    add comments

--- boot.asm
+++ boot.asm
@@ -1,7 +1,7 @@
 [BITS 16]
 
-cli
-cld
+cli; clear interrupt-enable flag
+cld; clear derection flag, if df = 0 string ooperations increment
 
 mov sp, 0x7C00 
 
@@ -17,33 +17,33 @@ RCLP:
     mov es, ax
     xor bx, bx
 read_sectors:
-    mov al, 0x12 
-    mov ah, 0x2
-    mov cl, 0x1
-    int 0x13 
-    jc read_sectors
+    mov al, 0x12; count of reading sectors
+    mov ah, 0x2; function - read sectors from driver
+    mov cl, 0x1; number of sector
+    int 0x13
+    jc read_sectors; c-flag if was error
     add bx, 0x2400
-    xor dh, 1
-    jnz read_sectors
-    inc ch
+    xor dh, 1; next head
+    jnz read_sectors; second iteration if dh = 0
+    inc ch; next number of cylinder
 write_buf:
-    call swap_segments
+    call swap_segments; swap ds and es
     xor si, si
     xor di, di
     mov ax, 0x2400
 .WBL:
-    movsw
+    movsw; move word(2 bytes) from address ds:si to es:di (from buffer to data)
     dec ax 
     jnz .WBL
     mov ax, es 
     add ax, 0x480
-    mov ds, ax
+    mov ds, ax; shift by 18 kb
     dec bp
     jnz RCLP
 
-hlt
+hlt; stop instruction execution
 
-swap_segments:
+swap_segments:; swap ds and es
     mov ax, ds
     mov si, es
     mov ds, si
