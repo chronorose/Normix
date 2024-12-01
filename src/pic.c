6ab7b8bec17ccd0caf53fb34b7497e9abc348fc5
ptntsv <hitogavax@ya.ru>
2024-12-01 18:04:53 +0700

    pic_unmask fix

--- src/pic.c
+++ src/pic.c
@@ -62,7 +62,7 @@ void pic_send_eoi(byte_t irq) {
 }
 
 void pic_unmask(byte_t irq) {
-    pic_send_byte(MASTER_DATA, inb(MASTER_DATA) & ~irq);
+    pic_send_byte(MASTER_DATA, inb(MASTER_DATA) & ~(1 << irq));
 }
 
 void pic_unmask_all(void) {
